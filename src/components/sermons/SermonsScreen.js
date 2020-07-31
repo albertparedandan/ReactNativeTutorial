b520be34dfb4ed5c87d26ec45b774f46831e0336
Albert Paredandan <albertparedandan@gmail.com>
2020-08-01 00:51:06 +0800

    added mainstacknavigator

---
+++ src/components/sermons/SermonsScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const SermonsScreen: () => React$Node = () => {
+  return (
+    <View>
+      <Text>TEST</Text>
+    </View>
+  )
+};
+
+export default SermonsScreen;

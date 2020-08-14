d233c9219a7ac7a0ab3fba381802f839181e3c0b
Albert Paredandan <albertparedandan@gmail.com>
2020-08-14 18:46:38 +0800

    added different sermons

--- src/components/sermons/SermonScreen.js
+++ src/components/sermons/SermonScreen.js
@@ -4,30 +4,31 @@ import TrackPlayer from 'react-native-track-player';
 import trackPlayerServices from '../../services/trackPlayerServices';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 
-const track = {
-  id: '1',
-  url: 'https://hongkong.hmcc.net/wp-content/uploads/sermons/Forward-Part3-2020-07-26-SermonAudio.mp3',
-  title: 'Forward Series Part 3',
-  artist: 'Pastor Bo Zhu',
-};
-
-const track1 = {
-  id: '2',
-  url: 'https://hongkong.hmcc.net/wp-content/uploads/sermons/Forward-Part2-2020-07-19-SermonAudio.mp3',
-  title: 'Forward Series Part 2',
-  artist: 'Pastor Seth S Kim',
-};
-
+// const track = { id: '1',
+//   url: 'https://hongkong.hmcc.net/wp-content/uploads/sermons/Forward-Part3-2020-07-26-SermonAudio.mp3',
+//   title: 'Forward Series Part 3',
+//   artist: 'Pastor Bo Zhu',
+// };
+// 
+// const track1 = {
+//   id: '2',
+//   url: 'https://hongkong.hmcc.net/wp-content/uploads/sermons/Forward-Part2-2020-07-19-SermonAudio.mp3',
+//   title: 'Forward Series Part 2',
+//   artist: 'Pastor Seth S Kim',
+// };
+// 
 TrackPlayer.setupPlayer().then(() => {
   console.log('setup');
   TrackPlayer.registerPlaybackService(() => trackPlayerServices);
-  TrackPlayer.add([track, track1]);
 });
 
-const SermonScreen = () => {
+export default function SermonScreen({ route, navigation }) {
+  const sermon = route.params;
+  const { title, id, url } = sermon;
+  TrackPlayer.add(sermon);
   return(
     <View>
-      <Text>SERMONSCREEN</Text>
+      <Text>{title}</Text>
       <TouchableOpacity onPress={() => TrackPlayer.play()}>
         <Text>Play</Text>
       </TouchableOpacity>
@@ -45,7 +46,4 @@ const SermonScreen = () => {
       </TouchableOpacity>
     </View>
   );
-};
-
-
-export default SermonScreen;
+}
--- src/components/sermons/SermonsScreen.js
+++ src/components/sermons/SermonsScreen.js
@@ -1,17 +1,87 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import {TouchableOpacity} from 'react-native-gesture-handler';
+import {TouchableOpacity, FlatList} from 'react-native-gesture-handler';
 import {useNavigation} from '@react-navigation/native';
+
+const DATA = [
+  {
+    id: '1',
+    title: 'Forward - Part 4: In Faith',
+    album: ['Forward'],
+    time: '10:00 AM',
+    artist: 'Pastor Seth S. Kim',
+    url:
+      'https://hongkong.hmcc.net/wp-content/uploads/sermons/Forward-Part4-2020-08-02-Sermon-Audio.mp3',
+    artwork:
+      'https://hongkong.hmcc.net/wp-content/uploads/forward-final-ad-min.jpg',
+  },
+  {
+    id: '2',
+    title: 'Forward - Part 3: In Partnership',
+    album: ['Forward'],
+    time: '10:00 AM',
+    artist: 'Pastor Bo Zhu',
+    url:
+      'https://hongkong.hmcc.net/wp-content/uploads/sermons/Forward-Part3-2020-07-26-SermonAudio.mp3',
+    artwork:
+      'https://hongkong.hmcc.net/wp-content/uploads/forward-final-ad-min.jpg',
+  },
+  {
+    id: '3',
+    title: 'Forward - Part 2: In Investment',
+    album: ['Forward'],
+    time: '10:00 AM',
+    artist: 'Pastor Seth S. Kim',
+    url:
+      'https://hongkong.hmcc.net/wp-content/uploads/sermons/Forward-Part2-2020-07-19-SermonAudio.mp3',
+    artwork:
+      'https://hongkong.hmcc.net/wp-content/uploads/forward-final-ad-min.jpg',
+  },
+  {
+    id: '4',
+    title: 'Forward - Part 1: In Obedience',
+    album: ['Forward'],
+    time: '10:00 AM',
+    artist: 'Pastor Seth S. Kim',
+    url:
+      'https://hongkong.hmcc.net/wp-content/uploads/sermons/Forward-Part1-2020-07-12-SermonAudio.mp3',
+    artwork:
+      'https://hongkong.hmcc.net/wp-content/uploads/forward-final-ad-min.jpg',
+  },
+  {
+    id: '5',
+    title: 'Steadfast - Part 7',
+    album: ['Steadfast'],
+    time: '10:00 AM',
+    artist: 'Pastor Seth S. Kim',
+    url:
+      'https://hongkong.hmcc.net/wp-content/uploads/sermons/Steadfast-Part7-2020-07-05-SermonAudio.mp3',
+    artwork:
+      'https://hongkong.hmcc.net/wp-content/uploads/steadfast-ad.png',
+  },
+];
+
+const Item = ({ title }) => (
+  <View>
+    <Text>{title}</Text>
+  </View>
+);
 
 const SermonsScreen: () => React$Node = () => {
   const navigation = useNavigation();
+  const renderItem = ({ item }) => (
+    <TouchableOpacity onPress={() => navigation.navigate('Test', item)}>
+      <Item title={item.title} />
+    </TouchableOpacity>
+  );
+
   return (
     <View>
-      <Text>Forward Series Part 3</Text>
-      <Text>Pastor Bo Zhu</Text>
-      <TouchableOpacity onPress={() => navigation.navigate('Test')}>
-        <Text>TEST</Text>
-      </TouchableOpacity>
+      <FlatList
+        data = {DATA}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
     </View>
   )
 };
